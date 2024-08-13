1afccd8ac603b738c2bd55884250b9433b68526f
Andrew121410 <andrewcherring@gmail.com>
2024-08-12 21:04:30 -0400

    Bump minecraft-server-ping again.

--- ccbot-core/build.gradle.kts
+++ ccbot-core/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     api("net.dv8tion:JDA:5.0.2")   // https://mvnrepository.com/artifact/net.dv8tion/JDA
     api("org.reflections:reflections:0.10.2") // https://mvnrepository.com/artifact/org.reflections/reflections
     api("com.github.andrew121410:CCUtilsJava:b3c9a23201")
-    api("com.github.andrew121410:minecraft-server-ping:a1039c4185")
+    api("com.github.andrew121410:minecraft-server-ping:2e7159a5ff")
 
     // Jackson -> https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core
     val jacksonVersion = "2.17.2"
