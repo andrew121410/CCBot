2b7518cc4df16eeb3054fd1b962df7fb6dd13719
Andrew121410 <andrewcherring@gmail.com>
2025-05-16 22:28:56 -0400

    Update CCUtilsJava

--- ccbot-core/build.gradle.kts
+++ ccbot-core/build.gradle.kts
@@ -7,7 +7,7 @@ description = "CCBot-Core"
 dependencies {
     api("net.dv8tion:JDA:5.5.1")   // https://mvnrepository.com/artifact/net.dv8tion/JDA
     api("org.reflections:reflections:0.10.2") // https://mvnrepository.com/artifact/org.reflections/reflections
-    api("com.github.andrew121410.CCUtilsJava:ccutilsjava-relocation:c37f2279a6")
+    api("com.github.andrew121410.CCUtilsJava:ccutilsjava-relocation:99569acaab")
     api("com.github.andrew121410:minecraft-server-ping:ba3404b3a0")
 
     // Jackson -> https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core
