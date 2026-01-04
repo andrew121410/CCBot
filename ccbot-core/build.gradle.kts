ef41c7ec396806c1648b049e8b7743df7efa41db
Andrew121410 <andrewcherring@gmail.com>
2026-01-04 15:41:06 -0500

    Update CCUtilsJava && minecraft-server-ping

--- ccbot-core/build.gradle.kts
+++ ccbot-core/build.gradle.kts
@@ -7,8 +7,8 @@ description = "CCBot-Core"
 dependencies {
     api("net.dv8tion:JDA:6.2.1")   // https://mvnrepository.com/artifact/net.dv8tion/JDA
     api("org.reflections:reflections:0.10.2") // https://mvnrepository.com/artifact/org.reflections/reflections
-    api("com.github.andrew121410.CCUtilsJava:ccutilsjava-relocation:08b1571b50")
-    api("com.github.andrew121410:minecraft-server-ping:0756645bb6")
+    api("com.github.andrew121410.CCUtilsJava:ccutilsjava-relocation:726f19c60a")
+    api("com.github.andrew121410:minecraft-server-ping:6cc532c374")
 
     // Jackson -> https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core
     val jacksonVersion = "2.20.1"
